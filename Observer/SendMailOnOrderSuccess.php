950dd9b6b406228807ef2a6daab591ca67a2ddd2
James Bramich <james.bramich@gmail.com>
2023-04-24 17:10:25 +1000

    moved (nofrixion) order email to post checkout

--- Observer/SendMailOnOrderSuccess.php
+++ Observer/SendMailOnOrderSuccess.php
@@ -6,6 +6,8 @@ use Magento\Framework\Event\ObserverInterface;
 
 class SendMailOnOrderSuccess implements ObserverInterface
 {
+    private $logger;
+
     /**
      * @var \Magento\Sales\Model\OrderFactory
      */
@@ -31,12 +33,13 @@ class SendMailOnOrderSuccess implements ObserverInterface
     public function __construct(
         \Magento\Sales\Model\OrderFactory $orderModel,
         \Magento\Sales\Model\Order\Email\Sender\OrderSender $orderSender,
-        \Magento\Checkout\Model\Session $checkoutSession
-    )
-    {
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Psr\Log\LoggerInterface $logger,
+    ) {
         $this->orderModel = $orderModel;
         $this->orderSender = $orderSender;
         $this->checkoutSession = $checkoutSession;
+        $this->logger = $logger;
     }
 
     /**
@@ -46,14 +49,18 @@ class SendMailOnOrderSuccess implements ObserverInterface
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         $orderIds = $observer->getEvent()->getOrderIds();
-        if(count($orderIds))
-        {
+        if (count($orderIds)) {
             $this->checkoutSession->setForceOrderMailSentOnSuccess(true);
             $order = $this->orderModel->create()->load($orderIds[0]);
 
-            // code here to check that payment is complete before sending the email.
-            
-            $this->orderSender->send($order, true);
+            $orderStatus = $order->getStatus();
+            $paymentMethodCode = $order->getPayment()->getMethod();
+
+            // Only send order confirmation (here) for completed NoFrixion payments.
+            if ($paymentMethodCode == 'nofrixion' && ($orderStatus === 'complete' || $orderStatus === 'nofrixion_paid_correctly')) {
+                $this->logger->info(__METHOD__ . ' Order: '. $order->getIncrementId() . ' - ' . $order->getStatus());
+                $this->orderSender->send($order, true);
+            }
         }
     }
 }
