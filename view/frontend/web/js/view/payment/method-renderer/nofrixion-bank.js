1fd4d366e9ea30776be0df07ef0aeb9fe5b904d1
James Bramich <james.bramich@gmail.com>
2023-07-14 11:23:46 +1000

    fixed processing animation

--- view/frontend/web/js/view/payment/method-renderer/nofrixion-bank.js
+++ view/frontend/web/js/view/payment/method-renderer/nofrixion-bank.js
@@ -1,15 +1,17 @@
 define(
     [
         'jquery',
+        'ko',
         'Magento_Customer/js/model/customer',
         'Magento_Ui/js/model/messages',
         'Magento_Checkout/js/view/payment/default'
     ],
-    function ($, Customer, Messages, Component) {
+    function ($, ko, Customer, Messages, Component) {
         'use strict';
         var self;
         var baseRedirectUrl = window.checkoutConfig.payment.nofrixion.paymentRedirectUrl;
         var payByBankProviderId = '';
+        //var buttonClicked = ko.observable(false);
         return Component.extend({
             defaults: {
                 template: 'Nofrixion_Payments/payment/nofrixion-bank'
@@ -18,14 +20,18 @@ define(
                 //initialize parent Component
                 this._super();
                 self = this;
+                self.payByBankProviders = window.checkoutConfig.payment.nofrixion.payByBankProviders;
             },
             initiatePayment: function (data, event) {
                 payByBankProviderId = data.personalInstitutionID;
-                //self.isProcessing =true;
+                self.isProcessing(true);
                 self.placeOrder(data, event);
             },
+            getPayByBankProviders: function () {
+                return self.payByBankProviders;
+            },
             isCustomerLoggedIn: Customer.isLoggedIn,
-            isProcessing: false,
+            isProcessing: ko.observable(false),
             redirectAfterPlaceOrder: false,
             afterPlaceOrder: function () {
                 var url = baseRedirectUrl + '?bankId=' + encodeURIComponent(payByBankProviderId);
